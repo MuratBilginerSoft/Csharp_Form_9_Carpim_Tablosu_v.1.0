42a01da6840c81ec3f48a6cfcf32acd26f20ec65
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:03:28 +0300

    Initial commit

---
+++ Kod-H Çarpım Tablosu v1.0 Gloster/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kod_H_Çarpım_Tablosu_v1._0_Gloster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    x = j * i;
+                    string a = i + "*" + j + "=" + x;
+                    listBox1.Items.Add(a);
+                }
+                listBox1.Items.Add("\n");
+            }
+        }
+    }
+}
